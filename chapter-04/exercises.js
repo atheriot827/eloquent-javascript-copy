15e559c7256aeee343db4199549f1ae13c34d2fe
atheriot827 <atheriot827@gmail.com>
2024-08-07 21:36:18 -0500

    complete Ch4 Range

--- chapter-04/exercises.js
+++ chapter-04/exercises.js
@@ -2,15 +2,46 @@
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function range() {
+//I: start, end
+//O: array containing all the numbers from start up to and including end
+//C:
+//E:
 
+function range(start, end, step = 1) {
+  //init emtpy array
+  let result = [];
+
+  //if start and end are the same
+  if(start === end) {
+    return result;
+  }
+
+  //positive step values
+  if(step > 0) {
+    for(let i = start; i <= end; i += step) {
+      result.push(i);
+    }
+  //neg step values
+  } else if ( step < 0) {
+    for(let i = start; i >= end; i += step) {
+      result.push(i);
+    }
+  }
+  return result;
 }
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function sum() {
+//I: array of numbers
+//O: returns the sum of these numbers
+//C:
+//E:
+
+function sum(array) {
 
 }
 
